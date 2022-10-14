79dc601908c91137b51caaba371b92b5c6586875
selly361 <104414846+selly361@users.noreply.github.com>
2022-10-14 10:17:41 +0100

    Update theme.ts

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,4 +1,19 @@
 export const theme = {
     
+colors: {
+    orange: "#D87D4A",
+    lightOrange: "#FBAF85",
+    darkGrey: "#101010",
+    black: "#000000",
+    gray: "#4C4C4C",
+    slate: "#888888",
+    white: "#FFFFFF",
+    silver: "#F1F1F1",
+    lightSilver: "#FAFAFA",
+}
+    
+ fonts: {
+   primaryFont: `'Manrope', sans-serif`,
+}
 
 }
