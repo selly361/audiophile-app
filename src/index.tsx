23f1b17dcf2d46a4f869fa5ee9bf81fa1cba7e06
selly361 <104414846+selly361@users.noreply.github.com>
2022-10-14 12:46:33 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@ import { Provider } from "react-redux";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { store } from "./app/store";
-
+import { store } from "app/store";
+import { ThemeProvider } from "styled-components";
+import { theme } from "global/theme";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ThemeProvider theme={theme}>
       <Router>
         <App />
       </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
