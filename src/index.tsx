1145155deef92e447a196bb5e4d96cc442ec45db
selly361 <104414846+selly361@users.noreply.github.com>
2022-10-14 12:49:20 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { store } from "app/store";
+import { store } from "./app/store";
 import { ThemeProvider } from "styled-components";
 import { theme } from "global/theme";
 const container = document.getElementById("root")!;
