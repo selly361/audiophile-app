3f02221066f02a8e68af5abbe6093a1a61663351
selly361 <104414846+selly361@users.noreply.github.com>
2022-10-14 10:37:16 +0100

    added styles of header to a new file

---
+++ src/components/Layout/Header/header-styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const StyledHeader = styled.header`
+  width: 100vw;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #191919;
+`;
+
+export const NavContainer = styled.nav`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
