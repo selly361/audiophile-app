8dd85bd4dd1c1816e4ec328e08951e678df5d0ef
selly361 <104414846+selly361@users.noreply.github.com>
2022-10-14 12:41:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { HomePage, ProductPage, ProductsDetailPage } from './pages'
 import { Route, Routes } from 'react-router-dom'
 
-import Footer from './components/Layout/Footer/Footer'
-import Header from './components/Layout/Header/Header'
-import Wrapper from './components/Wrapper/Wrapper'
+import Footer from 'components/Layout/Footer/Footer'
+import Header from 'components/Layout/Header/Header'
+import Wrapper from 'components/Wrapper/Wrapper'
 
 const App = () => {
   return (
