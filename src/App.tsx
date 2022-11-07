231e8d302e687517ef428174c1affe0cdece7fe6
selly361 <104414846+selly361@users.noreply.github.com>
2022-11-07 13:40:47 +0000

    changed order of components in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { CheckoutPage, HomePage, ProductPage, ProductsDetailPage } from './pages'
-import { Route, Routes } from 'react-router-dom'
+import {
+  CheckoutPage,
+  HomePage,
+  ProductPage,
+  ProductsDetailPage,
+} from "./pages";
+import { Route, Routes } from "react-router-dom";
 
-import Footer from 'components/Layout/Footer/Footer'
-import { Fragment } from "react"
-import Header from 'components/Layout/Header/Header'
-import Modals from 'components/Modals/Modals'
-import Wrapper from 'components/Wrapper/Wrapper'
+import Footer from "components/Layout/Footer/Footer";
+import { Fragment } from "react";
+import Header from "components/Layout/Header/Header";
+import Modals from "components/Modals/Modals";
+import Wrapper from "components/Wrapper/Wrapper";
 
 const App = () => {
   return (
- <Fragment>
-     <Wrapper>
-      <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/product_detail/:productName" element={<ProductsDetailPage />} />
-        <Route path="/:category" element={<ProductPage />}>
-        <Route path="*" element={<h1>NO PAGE FOUND</h1>} />
-        </Route>
-      </Routes>
-    </Wrapper>
-    <Modals />
- </Fragment>
-  )
-}
+    <Fragment>
+      <Wrapper>
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/:category" element={<ProductPage />}>
+            <Route
+              path="/product_detail/:productName"
+              element={<ProductsDetailPage />}
+            />
+          </Route>
+        </Routes>
+      </Wrapper>
+      <Modals />
+    </Fragment>
+  );
+};
 
-export default App
+export default App;
